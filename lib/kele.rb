1cff29d26b1ffcd0d852d6324714fbcd5a8b3c26
rrsg123 <rrsg123@gmail.com>
2017-05-01 15:20:41 -0600

    finish 7

--- lib/kele.rb
+++ lib/kele.rb
@@ -31,6 +31,10 @@ class Kele
     puts response
   end
 
+  def create_submissions(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    response = self.class.post(base_api_endpoint("checkpoint_submissions"), body: { "checkpoint_id": checkpoint_id, "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "comment": comment }, headers: { "authorization" => @auth_token })
+    puts response
+  end
   
   private
 
