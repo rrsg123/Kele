d2d3cc92197d83cea0e2dc67fc4111dbf0898784
rrsg123 <rrsg123@gmail.com>
2017-05-01 13:49:52 -0600

    finish 2

---
+++ lib/kele.rb
@@ -0,0 +1,17 @@
+require 'httparty'
+require 'kele'
+
+class Kele
+  include HTTParty
+  def initialize(email, password)
+    response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
+    raise "invalid email/pass" if response.code != 200
+    @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    url = "https://www.bloc.io/api/v1/users/me"
+    response = self.class.get(url, headers: { "authorization" => @auth_token })
+  end
+
+end
